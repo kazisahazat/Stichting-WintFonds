d9ad7d11b237c4bfec4ff595f362af46ff3294a3
Kazi Sahazat Islam <61798614+kazisahazat@users.noreply.github.com>
2023-12-04 00:10:55 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,27 @@
+$(function(){
+
+ 
+    
+//  if (screen.width>1500){document.write("<style>body{zoom:120%;}</style>");}
+ 
+
+
+//animation scroll js
+//    var html_body = $('html, body');
+//    $('.down a').on('click', function () {
+//        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+//            var target = $(this.hash);
+//            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+//            if (target.length) {
+//                html_body.animate({
+//                    scrollTop: target.offset().top - 0
+//                }, 1500);
+//                return false;
+//            }
+//        }
+//    });
+
+});
+
+
+
